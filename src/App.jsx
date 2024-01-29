28ebe0d71e9fdec081336bde132653193449b74d
devMomen162 <momenul.dev@gmail.com>
2024-01-30 02:13:34 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -65,13 +65,14 @@ const App = () => {
 
   return (
     <div>
-      <div className="md:w-1/2 mx-auto text-center bg-slate-200 md:px-10 py-20 shadow-lg rounded-lg">
+      <div className="px-2 md:w-1/2 mx-auto text-center bg-slate-200 md:px-10 py-10  md:py-20 shadow-lg rounded-lg">
         <h1 className="text-3xl mb-4">
-          Hello dear... <br />
-          <span className="text-2xl">This is a mini calculator</span>
+          Hello my dear... <br />
+          <span className="text-purple-600 ">Faria Jannat</span> <br />
+          <span className="text-2xl">This is your mini calculator</span>
         </h1>
         <div className="px-20 py-4 rounded-lg bg-white font-bold">{result}</div>
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-10 justify-center">
           <div>
             <h2 className="text-xl mb-2">Inputs</h2>
             <input
@@ -123,7 +124,7 @@ const App = () => {
               %
             </button>
             <button className="btn btn-outline bg-green-100 text-black" onClick={handleClear}>
-              Clear
+              AC
             </button>
           </div>
         </div>
